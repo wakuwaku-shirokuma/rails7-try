7dd9d7fc6eeff36ef56b810c51e63c42a56bfd3f
wakuwaku-shirokuma <wakuwaku.shirokuma.works@gmail.com>
2025-04-09 05:33:22 +0900

    postsリソースを削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2025_04_06_005301) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
